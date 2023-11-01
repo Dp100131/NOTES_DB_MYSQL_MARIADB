fee58c0059acb3531d45cf85418ca3b66538e741
Daniel Pinto <daniel2190558@correo.uis.edu.co>
2023-11-01 11:22:27 -0500

    INSERT INTO

---
+++ 4-insert-lines.sql
@@ -0,0 +1,18 @@
+USE metro_cmdx;
+
+
+INSERT INTO `lines` (name, color) VALUES
+("Línea 1", "Rosa"),
+("Línea 2", "Azul"),
+("Línea 3", "Verde olivo"),
+("Línea 4", "Cian"),
+("Línea 5", "Amarillo"),
+("Línea 6", "Rojo"),
+("Línea 7", "Naranja"),
+("Línea 8", "Verde"),
+("Línea 9", "Café"),
+("Línea A", "Morado"),
+("Línea B", "Verde y gris"),
+("Línea 12", "Oro");
+
+SELECT * FROM `lines`;
